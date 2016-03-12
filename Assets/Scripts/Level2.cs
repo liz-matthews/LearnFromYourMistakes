c353b6b7016fd074dcd9e22b0e2c60b00491f185
omeed12 <omeed12@ufl.edu>
2016-03-11 23:03:48 -0500

    Added second scene
    Adds second scene and transition to the second scene.

---
+++ Assets/Scripts/Level2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2 : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider hit) {
+		Application.LoadLevel("SecondSceneV1.0");
+	}
+}
