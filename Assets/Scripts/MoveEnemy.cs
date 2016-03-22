dfead614bb77992400b3e0db36b0c57012647512
EvilBhalu <goyalprateek.21@gmail.com>
2016-03-21 21:41:09 -0400

    Scene2
    Scene2 fixes

--- Assets/Scripts/MoveEnemy.cs
+++ Assets/Scripts/MoveEnemy.cs
@@ -4,9 +4,6 @@ using System.Collections;
 public class MoveEnemy : MonoBehaviour {
 
 	Vector3 pos;
-
-	public Transform playr;
-
 	// Use this for initialization
 	void Start () {
 		GetComponent<Animation>().Play ("ariseHigh");
@@ -15,16 +12,7 @@ public class MoveEnemy : MonoBehaviour {
 	}
 
 	IEnumerator waitTime(){
-		
 		yield return new WaitForSeconds (2.0f);
-		GetComponent<Animation> ().Play ("idleBreatheHigh");
-	}
-
-	IEnumerator biteTime(){
-		yield return new WaitForSeconds (1.25f);
-		transform.position = new Vector3 (500.0f, 0.0f, 519.7f);
-	//	GetComponent<Animation>().Play ("ariseHigh");
-	//	yield return new WaitForSeconds (5.0f);
 		GetComponent<Animation> ().Play ("idleBreatheHigh");
 	}
 
@@ -40,34 +28,28 @@ public class MoveEnemy : MonoBehaviour {
 
 	//Mudball Attack
 	void Mudball(){
-		GetComponent<Animation> ().wrapMode = WrapMode.Once;
 		GetComponent<Animation> ().Play ("spitHigh");
-
 		//Projectile code
 
 		//Damage code
-
-		StartCoroutine ("waitTime");
 	}
 
 	//Underground Bite attack
 	void UndergroundBite(){
-		transform.position = playr.transform.position;
+		//GetComponent<Animation> () ["disappearHigh"].wrapMode = WrapMode.Once;
+		//GetComponent<Animation> ().Play ("disappearHigh");
+
+		//pos = GameObject.Find("Player").transform.position;
+
 		GetComponent<Animation> ().Play ("UndergroundBite");
 		//Damage code
-
-		StartCoroutine ("biteTime");
 	}
-		
 	// Update is called once per frame
 	void Update () {
 		 //use it to test various animations
-		 	if (Input.GetKey("up"))
+		 	if (Input.GetKey("up")){
 			UndergroundBite ();
-
-			if (Input.GetKey("down"))
-				Mudball ();
-
+		}
 
 	}
 }
